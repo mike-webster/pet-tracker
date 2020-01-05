718ecb2a90eedfb69eba5a8995101803abb385ee
Mike Webster <mwebster@wyzant.com>
2020-01-05 12:47:41 -0600

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pet
+  resources :pet, :event
+
+  get "/healthcheck", to: "application#healthcheck"
+  get "/pet/:id/events", to: "pet#events"
 end
