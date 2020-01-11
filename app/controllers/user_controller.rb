29b92bb14c13a227b8b2a68de7152761df1b156a
Mike Webster <mwebster@wyzant.com>
2020-01-11 16:35:46 -0600

    bcrypt

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -36,6 +36,6 @@ class UserController < ApplicationController
     private
   
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :pass_hash)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 end
