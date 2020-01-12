c5f6b4d22230aa780471f00353d6d6e45dbf0d73
Mike Webster <mwebster@wyzant.com>
2020-01-12 12:49:01 -0600

    attach pet to user

--- app/controllers/pet_controller.rb
+++ app/controllers/pet_controller.rb
@@ -7,6 +7,7 @@ class PetController < ApplicationController
 
   def create
     @pet = Pet.new(pet_params)
+    @pet.user = @current_user
     if @pet.validate
       @pet.save!
       redirect_to pet_path(@pet.id)
