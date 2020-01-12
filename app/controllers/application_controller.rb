c51995731edee0a62818f7f8a7f69892a68f3164
Mike Webster <mwebster@wyzant.com>
2020-01-11 20:33:01 -0600

    implement auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,68 @@ class ApplicationController < ActionController::Base
     cookies.delete APP_CONFIG['auth_key'].to_sym
     redirect_to login_path
   end
+
+  def current_user
+    token = cookies[APP_CONFIG['auth_key']]
+    return if token.nil?
+    payload = JwtUtils.decode(token)
+    return if payload.nil?
+    @current_user = User.find_by(id: payload["user_id"])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authorized
+    redirect_to login_path unless logged_in?
+  end
+
+  def authorize_pet
+    if !logged_in?
+      Rails.logger.error(event: "unauthorized", message: "not logged in")
+      redirect_to login_path 
+      return
+    end
+
+    pet_id = params[:id]
+    pet = Pet.find_by(id: pet_id)
+    if pet.nil?
+      redirect_to pet_index_path
+      Rails.logger.error(event: "unauthorized", message: "pet doesnt exist")
+      return
+    end
+
+    unless pet.user.id == @current_user.id
+      redirect_to pet_index_path
+      Rails.logger.error(event: "unauthorized", message: "pet does not belong to user")
+      return
+    end
+    
+    Rails.logger.debug(event: "authorized_pet")
+  end
+
+  def authorize_event
+    if !logged_in?
+      redirect_to login_path 
+      Rails.logger.error(event: "unauthorized", message: "not logged in")
+      return
+    end
+
+    event_id = params[:id]
+    event = Event.find_by(id: event_id)
+    if event.present? && event.pet.user.id != @current_user.id
+      redirect_to pet_index_path
+      Rails.logger.error(event: "unauthorized", message: "event associated with pet that does not belong to user")
+      return
+    end
+    
+    pet_id = params[:pet_id]
+    pet = Pet.find_by(id: pet_id)
+    if pet.present? && pet.user.id != @current_user.id
+      redirect_to pet_index_path
+      Rails.logger.error(event: "unauthorized", message: "event - pet does not belong to user")
+      return
+    end
+  end
 end
