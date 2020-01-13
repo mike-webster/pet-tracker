94abbd3b857d0963f7581046d5e7534db331e690
Mike Webster <mwebster@wyzant.com>
2020-01-12 23:37:25 -0600

    add new event functionality

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,21 +2,26 @@ class EventController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
   before_action :authorize_event, only: %i[show]
 
+  def new
+    @event = Event.new
+    @event.happened_at = Time.now.utc
+    @pets = @current_user.pets
+    @events = @pets.map{|p| p.events}.flatten
+  end
+
   def create
     @event = Event.new(event_params)
     @event.created_at = Time.now.utc
     if @event.validate
       @event.save!
       Rails.logger.info(event: "created_event", object: @event)
-      render json: "{'message': 'created'}", status: 204
+      redirect_to new_event_path
       return
     end
 
-    errors = []
-    if @event.errors.messages.any?
-      errors = @event.errors.messages.map { |err| "Field Name: #{err[0]} -- Problems: #{err[1].split(',')}"}
-    end
-    render status: 400, json: JSON.parse(errors.to_s)
+    @pets = @current_user.pets
+    @events = @pets.map{|p| p.events}.flatten
+    render :new, status: 400
   end
 
   def show
