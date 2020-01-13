4b596511c199301faa67d7a3aa3bbcf03d55b182
Mike Webster <mwebster@wyzant.com>
2020-01-13 00:46:56 -0600

    only validate password on create for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
   validates :first_name, :last_name, length: { in: 1..50}
   validates :email, length: { in: 5..200 }
-  validates :password, length: { in: 10..500 }
+  validates :password, length: { in: 10..500 }, on: :create
 
 end
