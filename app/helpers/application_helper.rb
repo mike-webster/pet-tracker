f037d1256c9f728553e34ff4029ade61fe08502a
Mike Webster <mwebster@wyzant.com>
2020-01-13 00:59:40 -0600

    add view helper to show time in user's timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def show_local_time(time)
+    return time.strftime("%Y-%m-%d %H:%M %Z") if @current_user.nil? || @current_user.timezone.blank?
+    return time.in_time_zone(@current_user.timezone).strftime("%Y-%m-%d %H:%M %Z") 
+  end
 end
