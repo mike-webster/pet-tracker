6b5cfc5a9855846319dc70489fce1776218e7226
Mike Webster <mwebster@wyzant.com>
2020-01-28 21:49:28 -0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   has_many :pets
   has_secure_password
 
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, :email, :timezone, presence: true
   validates :first_name, :last_name, length: { in: 1..50}
   validates :email, length: { in: 5..200 }
   validates :password, length: { in: 10..500 }, on: :create
+  validate :timezone_not_default
+
+  def timezone_not_default
+    return if timezone != "Select timezone"
+    errors.add(:timezone, "please select a timezone")
+  end
 
 end
