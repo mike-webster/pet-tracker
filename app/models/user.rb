7c50a2680af151075f5de16540e688a0347aabde
Mike Webster <mwebster@wyzant.com>
2020-01-30 00:11:53 -0600

    remove duplicate validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :pets
   has_secure_password
 
-  validates :first_name, :last_name, :email, :timezone, presence: true
+  validates :timezone, presence: true
   validates :first_name, :last_name, length: { in: 1..50}
   validates :email, length: { in: 5..200 }
   validates :password, length: { in: 10..500 }, on: :create
