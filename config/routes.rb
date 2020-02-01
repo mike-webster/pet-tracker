16ae37a37400c06515ebba87a7890b07d772f2f7
Mike Webster <mwebster@wyzant.com>
2020-02-01 14:13:17 -0600

    add error routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/404', to: "errors#not_found", as: "err_not_found"
+  get '/500', to: "errors#internal_error", as: "err_internal"
+  
+  get "/healthcheck", to: "application#healthcheck"
+  get "/login", to: "application#login", as: "login"
+  post "/login", to: "application#login"
+  get "/logout", to: "application#logout", as: "logout"
+
   resources :pet, :event
   resources :user, only: [:new, :create]
-  get "/", to: "user#dashboard", as: "dashboard"
-  get "/crash", to: "application#crash"
-  get "/exception", to: "application#except"
+
+  get "/", to: "user#dashboard", as: "dashboard" 
   get "/user/edit", to: "user#edit"
   put "/user", to: "user#update"
   get  "/user", to:  redirect("/user/new")
 
-  get "/healthcheck", to: "application#healthcheck"
   get "/pet/:id/events", to: "pet#events", as: "pet_events"
-  get "/login", to: "application#login", as: "login"
-  post "/login", to: "application#login"
-  get "/logout", to: "application#logout", as: "logout"
 end
