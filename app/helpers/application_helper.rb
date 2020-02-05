187c980f39e5384b5bdc1a0d4e9cbd524fcab921
Mike Webster <mwebster@wyzant.com>
2020-02-05 01:42:02 -0600

    more small tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def show_local_time(time)
-    return time.strftime("%Y-%m-%d %H:%M %Z") if @current_user.nil? || @current_user.timezone.blank?
-    return time.in_time_zone(@current_user.timezone).strftime("%Y-%m-%d %H:%M %Z") 
+    time_format = "%Y-%m-%d %H:%M"
+    return time.strftime(time_format) if @current_user.nil? || @current_user.timezone.blank?
+    return time.in_time_zone(@current_user.timezone).strftime(time_format) 
   end
 end
