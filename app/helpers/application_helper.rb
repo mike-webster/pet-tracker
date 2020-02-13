59784092249c30b171d2851cb239d7b80b762ca6
Mike Webster <mwebster@wyzant.com>
2020-02-12 23:32:57 -0600

    add helper for chart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     return time.strftime(time_format) if @current_user.nil? || @current_user.timezone.blank?
     return time.in_time_zone(@current_user.timezone).strftime(time_format) 
   end
+
+  def show_line_chart(data)
+    line_chart data, 
+      xtitle: "Day", 
+      ytitle:"Time", 
+      xmin: Time.now.to_date - 5.days, 
+      xmax: Time.now.to_date + 1.day,
+      colors: ["#2BA84A", "#2D3A3A"]
+  end
 end
